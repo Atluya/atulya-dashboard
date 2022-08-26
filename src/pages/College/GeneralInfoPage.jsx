c7f82c54bfacc3c6f19a06bcc6032f66a09ccd8a
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-08-26 14:05:31 +0530

    instituteType

--- src/pages/College/GeneralInfoPage.jsx
+++ src/pages/College/GeneralInfoPage.jsx
@@ -89,7 +89,7 @@ export default function GeneralInfoPage() {
       },
       instituteType: {
         type: "string",
-        enum: ["Private", "Government"]
+        enum: ["Private", "Public"]
       }
     }
   };
